d9ef3d9c268d6843cd4df1ff620af55d92da71ca
kearnsmichael <kearnspmichael@gmail.com>
2021-04-12 20:01:03 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");
+});
+
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+});
+
+const colors = ["#85afb3", "#bcb198",  "#85afb3", 
+"#bcb198"];
+
+let i = 0;
+
+Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
+  item.style.cssText = `background-color: ${colors[i++]}`;
+});
+
+Array.from(document.querySelectorAll(".navigation-button")).forEach(item => {
+  item.onclick = () => {
+    item.parentElement.parentElement.classList.toggle("change");
+  };
+});
